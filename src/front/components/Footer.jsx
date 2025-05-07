3cd752c92d51f17c410e5025ebd654ffc22ea47c
MashiM0 <massimog.boschettib@gmail.com>
2025-05-07 18:30:19 +0000

    [Create ActivityCard, quickfix on Footer]

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -5,14 +5,14 @@ export const Footer = () => (
 		<div className="container-fluid d-flex justify-content-between mb-2">
 			<div className="col-3 d-flex justify-content-start px-4">
 				<img src="https://media.discordapp.net/attachments/873387605981544468/1369481575460241459/
-				LogoMystock.png?ex=681c049b&is=681ab31b&hm=582c044290e6d4ef2b5359786c8da8c3b83d8a4210e484ac75f909242b76109f&=&format=webp&quality=lossless" style={{ width: "7vw" }} />
+				LogoMystock.png?ex=681c049b&is=681ab31b&hm=582c044290e6d4ef2b5359786c8da8c3b83d8a4210e484ac75f909242b76109f&=&format=webp&quality=lossless" style={{ width: "7vw", height: "3vw" }} />
 			</div>
 			<div className="d-flex justify-content-end px-5">
-				<button type="button" class="btn" href="/">Homepage</button>
-				<button type="button" class="btn" href="/">Inicio</button>
-				<button type="button" class="btn" href="/">Configuración</button>
-				<button type="button" class="btn" href="/">Perfil</button>
-				<button type="button" class="btn" href="/">Contáctanos</button>
+				<button type="button" className="btn" href="/">Homepage</button>
+				<button type="button" className="btn" href="/">Inicio</button>
+				<button type="button" className="btn" href="/">Configuración</button>
+				<button type="button" className="btn" href="/">Perfil</button>
+				<button type="button" className="btn" href="/">Contáctanos</button>
 			</div>
 		</div>
 		<div className="container-fluid d-flex justify-content-between">
@@ -24,7 +24,7 @@ export const Footer = () => (
 			<div className="d-flex justify-content-end px-5 fs-4">
 				<button type="button" className="btn btn-link"><i className="fa-brands fa-square-facebook fs-2" /></button>
 				<button type="button" className="btn btn-link"><i className="fa-brands fa-square-instagram fs-2" /></button>
-				<button type="button" className="btn btn-link"><i class="fa-brands fa-square-twitter fs-2" /></button>
+				<button type="button" className="btn btn-link"><i className="fa-brands fa-square-twitter fs-2" /></button>
 			</div>
 		</div>
 		<div className="container border-top">
