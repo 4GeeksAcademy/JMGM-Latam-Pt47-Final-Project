f3c509ca858f654764d3c07e3cb7b57a491991e5
Madg26 <manux_852@hotmail.com>
2025-05-07 01:01:41 +0000

    compronente en contruccion

---
+++ src/front/components/EnConstruccion.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const EnConstruccion = () => {
+  return (
+    <div className="text-center mt-3 container">
+      <div className="en-construccion construccion">
+    </div>
+    </div>
+  )
+}
+
+export default EnConstruccion
