53d4c47c74e0476cc8fd24a0c3f7361ea1f9000e
chefdude99 <chefdude.1999@gmail.com>
2025-05-09 21:48:42 +0000

    LateralMenu adelantado

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -2,24 +2,14 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
-<<<<<<< HEAD
 import LateralMenu from "../components/LateralMenu"
 
-=======
-import EnConstruccion from "../components/EnConstruccion"
->>>>>>> db7b6ea60613c08e5d5d595385fbbff0a7f72c1e
-
-// Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
-<<<<<<< HEAD
         <ScrollToTop>
             <Navbar />
             <LateralMenu />
             <Footer />
         </ScrollToTop>
-=======
-       <EnConstruccion/>
->>>>>>> db7b6ea60613c08e5d5d595385fbbff0a7f72c1e
     )
 }
