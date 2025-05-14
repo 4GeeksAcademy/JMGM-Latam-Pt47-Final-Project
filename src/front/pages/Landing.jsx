cad0404a77bc3ca04bea8b43ff2b62606dff5496
MashiM0 <massimog.boschettib@gmail.com>
2025-05-14 02:20:05 +0000

    [created FeatureCard component]

--- src/front/pages/Landing.jsx
+++ src/front/pages/Landing.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import LandingBanner from '../components/LandingBanner'
+import FeatureCard from '../components/FeatureCard'
 
 export const Landing = () => {
     return (
         <>
             <LandingBanner />
-            <div className='mt-4'>
+            <div className='my-4'/>
+            <div className='pt-4'>
                 <h2 className='fw-bold text-center' style={{color:"#6C11D9"}}>Transforma la manera en la que gestionas tu inventario</h2>
+            </div>
+            <div className='d-flex p-4 mx-2 justify-content-evenly'>
+                <FeatureCard className="col-4"/>
+                <FeatureCard className="col-4"/>
+                <FeatureCard className="col-4"/>
             </div>
         </>
 
